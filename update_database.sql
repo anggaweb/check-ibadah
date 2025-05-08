962358bd7d9a2d744669ea0b8334bb097a7d849d
Angga Faot <anggafaot6@gmail.com>
2025-05-08 09:36:56 +0700

    new upload

--- update_database.sql
+++
@@ -1,7 +0,0 @@
--- Add signature and responsible_person columns to checklists table
-ALTER TABLE checklists ADD COLUMN signature LONGTEXT NULL;
-ALTER TABLE checklists ADD COLUMN responsible_person VARCHAR(255) NULL;
-
--- Add is_parent and parent_id columns to checklist_items table
-ALTER TABLE checklist_items ADD COLUMN is_parent TINYINT(1) DEFAULT 0;
-ALTER TABLE checklist_items ADD COLUMN parent_id INT(11) NULL;
--- update_database_remark.sql
+++
@@ -1,2 +0,0 @@
--- Add remark column to checklists table if it doesn't exist
-ALTER TABLE checklists ADD COLUMN IF NOT EXISTS remark TEXT NULL;
